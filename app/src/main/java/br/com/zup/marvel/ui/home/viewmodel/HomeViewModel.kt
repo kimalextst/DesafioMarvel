42bf5e46ebe67b66fa88af5bcb71a02991cf0f16
kimalextst <kimtorresst@gmail.com>
2022-07-26 12:07:45 -0300

    Entrega Minima

--- app/src/main/java/br/com/zup/marvel/ui/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/br/com/zup/marvel/ui/home/viewmodel/HomeViewModel.kt
@@ -5,8 +5,12 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import br.com.zup.marvel.data.model.Marvel
 import br.com.zup.marvel.data.repository.MarvelRepository
+import br.com.zup.marvel.data.repository.AuthenticationRepository
 
 class HomeViewModel: ViewModel() {
+
+    private val authenticationRepository = AuthenticationRepository()
+
     private val marvelRepository = MarvelRepository()
 
     private var _marvelListState = MutableLiveData<List<Marvel>>()
@@ -16,4 +20,8 @@ class HomeViewModel: ViewModel() {
        val listMarvel =  marvelRepository.getMarvelList()
         _marvelListState.value = listMarvel
     }
+
+    fun getUserName() = authenticationRepository.getNameUser()
+
+    fun logout() = authenticationRepository.logoutOut()
 }
